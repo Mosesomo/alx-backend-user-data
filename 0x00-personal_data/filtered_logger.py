15246ef5591de91ebec49b8c820046dad0c1f8d7
Mosesomo <moseswamboga4@gmail.com>
2024-04-11 19:33:31 +0300

    logging data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 '''Personal data'''
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str],
+                 redaction: str, message: str,
+                 separator: str) -> str:
     '''Return obfuscated log message'''
     for field in fields:
         message = re.sub(field+'=.*?'+separator,
