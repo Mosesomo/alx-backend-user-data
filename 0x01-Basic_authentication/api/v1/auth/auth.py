dbc936e8977101da4ba7c7e084bcac7ab473bbea
Mosesomo <moseswamboga4@gmail.com>
2024-04-16 18:14:41 +0300

    Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,12 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         '''Public method'''
-        return None
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''public method'''
