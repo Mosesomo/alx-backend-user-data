d5453d69ea8e50217783e8b3ec53b73c28634131
Mosesomo <moseswamboga4@gmail.com>
2024-04-17 00:03:36 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''Basic auth'''
+import base64
 from .auth import Auth
 
 
@@ -20,3 +21,19 @@ class BasicAuth(Auth):
             return None
         tok = authorization_header.split(" ")[-1]
         return tok
+
+    def decode_base64_authorization_header(
+                                           self,
+                                           base64_authorization_header: str
+                                          ) -> str:
+        '''that returns the decoded value of a Base64 string'''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decode = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(decode)
+            return decoded.decode("utf-8")
+        except Exception:
+            return None
