c736adc4ab15222530b566868cf5830606525c7e
Mosesomo <moseswamboga4@gmail.com>
2024-04-18 22:44:56 +0300

    session authenicatin

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Authorization'''
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''Auth class'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''public method'''
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths:
+            return False
+        if path.endswith('/'):
+            path += '/'
+        for excluded_path in excluded_paths:
+            if path == excluded_path.rstrip('/'):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        '''Public method'''
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''public method'''
+        return None
